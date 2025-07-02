e37ced970a8e87ba2173995e1ad590497a96b217
kaki-studios <kkirvela@gmail.com>
2025-07-02 11:13:03 +0300

    check in

--- private_dot_config/nvim/lua/configs/lspconfig.lua
+++ private_dot_config/nvim/lua/configs/lspconfig.lua
@@ -5,12 +5,26 @@ local servers = {
   cssls = {},
   gopls = {},
   rust_analyzer = {},
-  pyright = {}
+  pyright = {},
+  zls = {
+    cmd = {
+      "/home/kaki/Documents/custom_zls/zls",
+    },
+    enable_build_on_save = true,
+    build_on_save_step = "check",
+  },
+  clangd = {
+    cmd = {
+      "clangd",
+      "--background-index",
+      "--compile-commands-dir=build", --Point to your build dir
+      "--query-driver=/usr/bin/clang++",
+    },
+    filetypes = { "c", "cpp", "h", "hpp" },
+  },
 }
 
-
-
 for name, opts in pairs(servers) do
-  vim.lsp.enable(name)  -- nvim v0.11.0 or above required
+  vim.lsp.enable(name) -- nvim v0.11.0 or above required
   vim.lsp.config(name, opts) -- nvim v0.11.0 or above required
 end
--- private_dot_config/nvim/lua/plugins/init.lua
+++ private_dot_config/nvim/lua/plugins/init.lua
@@ -1,7 +1,7 @@
 return {
   {
     "stevearc/conform.nvim",
-    event = 'BufWritePre', -- uncomment for format on save
+    event = "BufWritePre", -- uncomment for format on save
     opts = require "configs.conform",
   },
 
@@ -12,23 +12,65 @@ return {
       require "configs.lspconfig"
     end,
   },
+  {
+    "rcarriga/nvim-dap-ui",
+    event = "VeryLazy",
+    dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
+    config = function()
+      local dap = require "dap"
+      local dapui = require "dapui"
+      dapui.setup()
+      dap.listeners.after.event_initialized["dapui_confing"] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_confing"] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+      end
+    end,
+  },
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "mfussenegger/nvim-dap",
+    },
+    opts = {
+      handlers = {},
+      ensure_installed = {
+        "codelldb",
+      },
+    },
+  },
+  {
+    "mfussenegger/nvim-dap",
+  },
 
   -- test new blink
   -- { import = "nvchad.blink.lazyspec" },
 
   {
-  	"nvim-treesitter/nvim-treesitter",
-  	opts = {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
 
       highlight = {
         enable = true,
       },
       auto_install = true,
 
-  		ensure_installed = {
-  			"vim", "lua", "vimdoc",
-       "html", "css", "go", "rust"
-  		},
-  	},
+      ensure_installed = {
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
+        "go",
+        "rust",
+        "zig",
+      },
+    },
   },
 }
